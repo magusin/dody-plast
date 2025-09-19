43797fb836c18efd74893b274ece1748881f683e
Hugo JACQUES <fanatsy4@gmail.com>
2025-09-19 12:43:33 +0200

    fix link  eslint

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
